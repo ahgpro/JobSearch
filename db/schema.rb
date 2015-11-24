5e72fa576c3c421086a1a23e2f53bb95691d932f
Herit Alexandre <apherit@gmail.com>
2015-11-24 12:15:56 +0100

    removed count from projectjob

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124111321) do
+ActiveRecord::Schema.define(version: 20151124111522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20151124111321) do
   create_table "project_jobs", force: :cascade do |t|
     t.integer  "project_id"
     t.integer  "job_id"
-    t.integer  "count"
     t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
