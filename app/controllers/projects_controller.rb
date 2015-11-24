964c856ecaeaa35d8624467b239da2aea9bbd66c
Herit Alexandre <apherit@gmail.com>
2015-11-24 14:29:36 +0100

    index + images

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,43 @@
 class ProjectsController < ApplicationController
+
+ before_action :find_project, only: [:show, :destroy]
+
+  def index
+    @projects = Project.all
+
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      render :new
+    end
+  end
+
+
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+
 end
