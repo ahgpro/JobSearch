7795d85bc4ef4f7485f9ce7445351ea2886ebc29
Herit Alexandre <apherit@gmail.com>
2015-11-24 12:14:18 +0100

    faker install, seed done, migration on projectjob, user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124110639) do
+ActiveRecord::Schema.define(version: 20151124111321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20151124110639) do
     t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "number"
   end
 
   add_index "project_jobs", ["job_id"], name: "index_project_jobs_on_job_id", using: :btree
