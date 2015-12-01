8612260e6bbc6538babe9625ab1ae45526fa3281
Herit Alexandre <apherit@gmail.com>
2015-12-01 15:27:01 +0100

    form done + js remove

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,25 +2,25 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-var $headline = $('.headline'),
-    $inner = $('.inner'),
-    $nav = $('nav'),
-    navHeight = 75;
+// var $headline = $('.headline'),
+//     $inner = $('.inner'),
+//     $nav = $('nav'),
+//     navHeight = 75;
 
-$(window).scroll(function() {
-  var scrollTop = $(this).scrollTop(),
-      headlineHeight = $headline.outerHeight() - navHeight,
-      navOffset = $nav.offset().top;
+// $(window).scroll(function() {
+//   var scrollTop = $(this).scrollTop(),
+//       headlineHeight = $headline.outerHeight() - navHeight,
+//       navOffset = $nav.offset().top;
 
-  $headline.css({
-    'opacity': (1 - scrollTop / headlineHeight)
-  });
-  $inner.children().css({
-    'transform': 'translateY('+ scrollTop * 0.4 +'px)'
-  });
-  if (navOffset > headlineHeight) {
-    $nav.addClass('scrolled');
-  } else {
-    $nav.removeClass('scrolled');
-  }
-});
+//   $headline.css({
+//     'opacity': (1 - scrollTop / headlineHeight)
+//   });
+//   $inner.children().css({
+//     'transform': 'translateY('+ scrollTop * 0.4 +'px)'
+//   });
+//   if (navOffset > headlineHeight) {
+//     $nav.addClass('scrolled');
+//   } else {
+//     $nav.removeClass('scrolled');
+//   }
+// });
