e8b169b6c7525413027cbfc070c7810a136fa7fa
Florent Allain <allain.florent@gmail.com>
2015-12-03 19:54:06 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
 
   root to: 'pages#home'
+  get '/contact', to: 'pages#contact'
+  get '/about', to: 'pages#about'
 
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   # , controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
@@ -19,5 +21,3 @@ Rails.application.routes.draw do
 
 
 end
-
-
